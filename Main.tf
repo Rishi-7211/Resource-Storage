e46428f586457f730859de40a43b028f7afc38db
Rishikesh Sharma <srishikesh39@gmai.com>
2025-07-22 09:57:04 +0530

    RG&storage acocunt code

---
+++ Main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "Rishi_RG"
+    storage_account_name = "storage7211"
+    container_name       = "terraformconatiner"
+    key                  = "prod.terraform.tfstate"
+  }
+
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "16a05a8e-7ba4-486d-8521-57e717aedbbd"
+}
+resource "azurerm_resource_group" "motorg" {
+  name     = "MotoRG"
+  location = "East US"
+}
+resource "azurerm_storage_account" "motoacc" {
+  name                     = "motostorageaccount98"
+  resource_group_name      = "MotoRG"
+  location                 = "West US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  depends_on = [ azurerm_resource_group.motorg ]
+}
+resource "azurerm_storage_container" "motocontainer" {
+  name                  = "moto-container"
+  storage_account_id    = azurerm_storage_account.motoacc.id
+  container_access_type = "private"
+  depends_on = [ azurerm_storage_account.motoacc ]
+}
