2bc2c77c20850eafffbe31cc77c9fe2789342535
Rishi-7211 <srishikesh39@gmail.com>
2025-08-04 10:40:30 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -5,7 +5,14 @@ terraform {
       version = "4.38.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "Vaishanvi_RG"
+    storage_account_name  = "vaishanvistorage"
+    container_name        = "vaishanvicontainer"
+    key                   = "prod.terraform.tfstate"
+  }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = "2152b8fd-4dc5-47a1-8df7-361e1b4e4a6f"
