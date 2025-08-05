84bc0fc6f6009274fd3df519cb51a7785c719fa9
Rishi-7211 <srishikesh39@gmail.com>
2025-08-05 10:25:27 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -15,6 +15,24 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "2152b8fd-4dc5-47a1-8df7-361e1b4e4a6f"
+  subscription_id = "50fb151d-0992-4631-9cc9-86f052a3e0b0"
 }
-
+}
+resource "azurerm_resource_group" "motorg" {
+  name     = "MotoRG1"
+  location = "East US"
+}
+resource "azurerm_storage_account" "motoacc" {
+  name                     = "motostorageaccount981"
+  resource_group_name      = "MotoRG1"
+  location                 = "West US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+depends_on = [azurerm_resource_group.motorg]
+}
+resource "azurerm_storage_container" "motocontainer" {
+  name                  = "moto-container"
+  storage_account_id    = azurerm_storage_account.motoacc.id
+  container_access_type = "private"
+depends_on = [azurerm_storage_account.motoacc]
+}
